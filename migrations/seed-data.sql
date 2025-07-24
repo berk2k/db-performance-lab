49ba700bd37dc3d6e548e40cbffbf6c7f7ec81ab
Berk Polat <berkpolat125@gmail.com>
2025-07-24 18:15:01 +0200

    seed data

--- migrations/seed-data.sql
+++ migrations/seed-data.sql
@@ -4,4 +4,4 @@ SELECT
   (random()*1000)::numeric(10,2) AS amount,
   CASE WHEN random() > 0.5 THEN 'COMPLETED' ELSE 'PENDING' END AS status,
   'Test transaction' AS description
-FROM generate_series(1, 100000);
+FROM generate_series(1, 100);
