fa0fde15796e16fe30ed68a9ecee2c1a611a457e
Berk Polat <berkpolat125@gmail.com>
2025-07-24 17:54:46 +0200

    seed data added

---
+++ migrations/seed-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO transactions (user_id, amount, status, description)
+SELECT
+  (random()*1000)::int AS user_id,
+  (random()*1000)::numeric(10,2) AS amount,
+  CASE WHEN random() > 0.5 THEN 'COMPLETED' ELSE 'PENDING' END AS status,
+  'Test transaction' AS description
+FROM generate_series(1, 100000);
